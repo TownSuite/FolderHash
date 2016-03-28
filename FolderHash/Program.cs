922efe5adef83d543195913a58ccaebc4c915e21
Peter Gill <peter.gill@townsuite.com>
2016-03-28 10:10:45 -0230

    SHA512 folder hash

---
+++ FolderHash/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSha512FolderHash(args[0]));
+        }
+
+        public static string GetSha512FolderHash(string path)
+        {         
+            var files = System.IO.Directory.GetFiles(path, "*.*", System.IO.SearchOption.AllDirectories);
+            Array.Sort(files);
+            using (var sha = new SHA512Managed())
+            {
+                int i = 0;
+                while (i < files.Length)
+                {
+                    var file = files[i];
+                    // hash path
+                    var relativePath = file.Substring(path.Length + 1);
+                    var pathBytes = Encoding.UTF8.GetBytes(relativePath.ToLower());
+                    sha.TransformBlock(pathBytes, 0, pathBytes.Length, pathBytes, 0);
+
+                    // hash contents
+                    var contentBytes = System.IO.File.ReadAllBytes(file);
+                    if (i == files.Length - 1)
+                    {
+                        sha.TransformFinalBlock(contentBytes, 0, contentBytes.Length);
+                    }
+                    else
+                    {
+                        sha.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+                    }
+                    i += 1;
+                }
+                return BitConverter.ToString(sha.Hash).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+Calculate and print SHA512 hash of folder and all contents.
+
+Example
+
+```bash
+FolderHash.exe "C:\Path\To\Folder"
+```
